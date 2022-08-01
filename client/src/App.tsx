e615a26f12fd9fb3830886af19be986202e2a818
jaimetrovoada <jaimetrovoada@gmail.com>
2022-08-01 23:24:03 +0800

    add frontend palette switching logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,15 +5,23 @@ import "./index.css";
 function App() {
   const [fileSelected, setSelectedFile] = React.useState<File>();
   const [uploadSuccess, setUploadSuccess] = React.useState<boolean>();
-  const [gruvFile, setGruvFile] = React.useState<any>();
+  const [gruvFile, setGruvFile] = React.useState<string[]>([]);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [palette, setPalette] = React.useState<string>("pink");
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUploadSuccess(undefined);
-    setGruvFile(undefined);
+    setGruvFile([]);
     if (e.target.files && e.target.files[0]) {
       setSelectedFile(e.target.files[0]);
     }
+  };
+
+  const handlePaletteChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    e.preventDefault();
+    setUploadSuccess(undefined);
+    setGruvFile([]);
+    setPalette(e.target.value);
   };
 
   const onFileUpload = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
@@ -23,7 +31,10 @@ function App() {
       setIsLoading(true);
 
       axios
-        .post(`${process.env.REACT_APP_UPLOADER_SERVER}/upload`, formData)
+        .post(
+          `${process.env.REACT_APP_UPLOADER_SERVER}/upload?palette=${palette}`,
+          formData
+        )
         .then((res) => {
           console.log(res);
           setUploadSuccess(true);
@@ -41,21 +52,65 @@ function App() {
   console.log("Upload Success", uploadSuccess);
   React.useEffect(() => {
     if (uploadSuccess) {
-      axios({
-        url: `${process.env.REACT_APP_UPLOADER_SERVER}/image?file=gruvbox_${fileSelected?.name}`,
-        method: "GET",
-        responseType: "blob",
-        headers: { "Content-Type": "image/*" },
-      })
-        .then((response) => {
-          const imageUrl = URL.createObjectURL(response.data);
-          setGruvFile(imageUrl);
-        })
-        .catch((err) => {
-          console.log(err);
+      if (palette === "both") {
+        const req1 = axios({
+          url: `${process.env.REACT_APP_UPLOADER_SERVER}/image?file=gruvbox_pink_${fileSelected?.name}`,
+          method: "GET",
+          responseType: "blob",
+          headers: { "Content-Type": "image/*" },
         });
+
+        const req2 = axios({
+          url: `${process.env.REACT_APP_UPLOADER_SERVER}/image?file=gruvbox_white_${fileSelected?.name}`,
+          method: "GET",
+          responseType: "blob",
+          headers: { "Content-Type": "image/*" },
+        });
+        axios
+          .all([req1, req2])
+          .then(
+            axios.spread((...responses) => {
+              const responseOne = responses[0];
+              const responseTwo = responses[1];
+              // use/access the results
+              const imageUrlOne = URL.createObjectURL(responseOne.data);
+              const imageUrlTwo = URL.createObjectURL(responseTwo.data);
+              setGruvFile([imageUrlOne, imageUrlTwo]);
+            })
+          )
+          .catch((err) => {
+            console.log(err);
+          });
+        /*   axios({
+          url: `${process.env.REACT_APP_UPLOADER_SERVER}/image?file=gruvbox_${fileSelected?.name}`,
+          method: "GET",
+          responseType: "blob",
+          headers: { "Content-Type": "image/*" },
+        })
+          .then((response) => {
+            const imageUrl = URL.createObjectURL(response.data);
+            setGruvFile([imageUrl]);
+          })
+          .catch((err) => {
+            console.log(err);
+          }); */
+      } else {
+        axios({
+          url: `${process.env.REACT_APP_UPLOADER_SERVER}/image?file=gruvbox_${fileSelected?.name}`,
+          method: "GET",
+          responseType: "blob",
+          headers: { "Content-Type": "image/*" },
+        })
+          .then((response) => {
+            const imageUrl = URL.createObjectURL(response.data);
+            setGruvFile([imageUrl]);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     }
-  }, [fileSelected?.name, uploadSuccess]);
+  }, [fileSelected?.name, palette, uploadSuccess]);
 
   console.log({ isLoading });
   const getStatus = (): string => {
@@ -76,6 +131,7 @@ function App() {
     return "Please select a file to upload";
   };
   console.log({ gruvFile });
+  console.log({ palette });
 
   return (
     <div className="App">
@@ -92,12 +148,18 @@ function App() {
             onChange={onFileChange}
             className="btn btn-filepicker"
           />
-
-          <button className="btn btn-upload" onClick={onFileUpload}>
-            <i className="fa fa-upload" />
-            Upload
-          </button>
         </label>
+        <select name="palette" id="palette" onChange={handlePaletteChange}>
+          <option value="pink" selected>
+            Pink/Panther
+          </option>
+          <option value="white">White/Snoopy</option>
+          <option value="both">Both</option>
+        </select>
+        <button className="btn btn-upload" onClick={onFileUpload}>
+          <i className="fa fa-upload" />
+          Upload
+        </button>
         <span>Status: {getStatus()}</span>
         {isLoading ? (
           <>
@@ -114,18 +176,45 @@ function App() {
             <img src={URL.createObjectURL(fileSelected)} alt="Upload" />
           </div>
         )}
-        {gruvFile ? (
-          <div>
-            <img src={gruvFile} alt="Upload" />
-            <a
-              className="buttonContainer"
-              download={`gruv_${fileSelected?.name}`}
-              href={gruvFile}
-            >
-              <i className="fa fa-download" />
-              &nbsp;Download
-            </a>
-          </div>
+        {gruvFile && gruvFile.length > 0 ? (
+          palette === "both" ? (
+            <>
+              <div>
+                <img src={gruvFile[0]} alt="Upload" />
+                <a
+                  className="buttonContainer"
+                  download={`gruv_${fileSelected?.name}`}
+                  href={gruvFile[0]}
+                >
+                  <i className="fa fa-download" />
+                  &nbsp;Download
+                </a>
+              </div>
+              <div>
+                <img src={gruvFile[1]} alt="Upload" />
+                <a
+                  className="buttonContainer"
+                  download={`gruv_${fileSelected?.name}`}
+                  href={gruvFile[1]}
+                >
+                  <i className="fa fa-download" />
+                  &nbsp;Download
+                </a>
+              </div>
+            </>
+          ) : (
+            <div>
+              <img src={gruvFile[0]} alt="Upload" />
+              <a
+                className="buttonContainer"
+                download={`gruv_${fileSelected?.name}`}
+                href={gruvFile[0]}
+              >
+                <i className="fa fa-download" />
+                &nbsp;Download
+              </a>
+            </div>
+          )
         ) : null}
       </div>
     </div>
